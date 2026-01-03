74466faca67c1cc1fce1a9176eef6ffce857b521
pepeuloureirob <pepeu.loureirob@gmail.com>
2026-01-02 22:43:26 -0300

    Update chamados.js

--- static/chamados.js
+++ static/chamados.js
@@ -1,1 +1,22 @@
+function carregar(restaurante){
+  setInterval(()=>{
+    fetch(`/status/${restaurante}`)
+      .then(r=>r.json())
+      .then(d=>{
+        let html=""
+        for (let m in d){
+          html+=`<button onclick="atender('${m}')">Mesa ${m}</button>`
+        }
+        document.getElementById("mesas").innerHTML = html || "Nenhum chamado"
+      })
+  },3000)
+}
+
+function atender(mesa){
+  fetch("/atender",{
+    method:"POST",
+    headers:{"Content-Type":"application/x-www-form-urlencoded"},
+    body:"restaurante="+restaurante+"&mesa="+mesa
+  })
+}
 
